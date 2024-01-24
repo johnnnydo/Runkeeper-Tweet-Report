ebb0914ae5b01b8ef15c8b811126860c63af5dda
Johnny Do <dojohnny847@gmail.com>
2024-01-24 15:09:55 -0800

    Finished the first point where we had to change the first and last date for the span

--- js/about.js
+++ js/about.js
@@ -12,6 +12,24 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	
+	//source https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+	var options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	};
+	//have to make variables can't put numbertweets-1 directly
+	var firstdate;
+	var lastdate;
+	//the tweet array is reversed latest ot oldest
+	lastdate = tweet_array[0].time;
+	firstdate = tweet_array[tweet_array.length-1].time;
+	/* document.getElementById('firstDate').innerText = firstdate.toLocaleDateString('en-US',options ); 
+	document.getElementById('lastDate').innerText = lastdate.toLocaleDateString('en-US',options );  */
+ 	$('#firstDate').text(firstdate.toLocaleDateString('en-US',options ));
+	$('#lastDate').text(lastdate.toLocaleDateString('en-US',options));
 }
 
 //Wait for the DOM to load
