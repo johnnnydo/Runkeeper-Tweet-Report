01e0e8c3ee2e6a05fa1f65d3c9a369f0ecd74e84
Johnny Do <dojohnny847@gmail.com>
2024-02-04 22:40:58 -0800

    Haven't commited in a while, but I finished part 2 of the project which was implementing the graphs. I had to debug my distance function in tweet.js and had to slowly learn to implement the data for vega lite and what data we  can use

--- js/activities.js
+++ js/activities.js
@@ -1,31 +1,264 @@
 function parseTweets(runkeeper_tweets) {
-	//Do not proceed if no tweets loaded
-	if(runkeeper_tweets === undefined) {
-		window.alert('No tweets returned');
-		return;
-	}
-	
-	tweet_array = runkeeper_tweets.map(function(tweet) {
-		return new Tweet(tweet.text, tweet.created_at);
-	});
+  //Do not proceed if no tweets loaded
+  if (runkeeper_tweets === undefined) {
+    window.alert("No tweets returned");
+    return;
+  }
 
-	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
+  tweet_array = runkeeper_tweets.map(function (tweet) {
+    return new Tweet(tweet.text, tweet.created_at);
+  });
 
-	activity_vis_spec = {
-	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-	  "description": "A graph of the number of Tweets containing each type of activity.",
-	  "data": {
-	    "values": tweet_array
-	  }
-	  //TODO: Add mark and encoding
-	};
-	vegaEmbed('#activityVis', activity_vis_spec, {actions:false});
+  document.getElementById("numberActivities").textContent = "9";
+  document.getElementById("firstMost").textContent = "running";
+  document.getElementById("secondMost").textContent = "biking";
+  document.getElementById("thirdMost").textContent = "walking";
 
-	//TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
-	//Use those visualizations to answer the questions about which activities tended to be longest and when.
+  //TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
+  let numTweetPerActivity = new Map();
+  for (let i = 0; i < tweet_array.length; i++) {
+    let activ = tweet_array[i].activityType;
+    if (!numTweetPerActivity.has(activ)) {
+      numTweetPerActivity.set(activ, 1);
+    } else {
+      let countCurrActivity = numTweetPerActivity.get(activ);
+      numTweetPerActivity.set(activ, countCurrActivity + 1);
+    }
+  }
+  //this is to get largest distance
+  let numTotalDistance = new Map(); //hhave to do total distance because we are making graphs based on the top 3 distance
+  for (let i = 0; i < tweet_array.length; i++) {
+    let activ = tweet_array[i].activityType;
+    if (!numTotalDistance.has(activ)) {
+      numTotalDistance.set(activ, tweet_array[i].distance);
+    } else {
+      let currAcitivyDistance = numTotalDistance.get(activ);
+      numTotalDistance.set(
+        activ,
+        currAcitivyDistance + tweet_array[i].distance
+      );
+    }
+  }
+  /* console.log(numTotalDistance); */
+  let activityData = [];
+  numTweetPerActivity.forEach((count, activity) => {
+    activityData.push({ activity: activity, tweets: count });
+  });
+  /* console.log(activityData); */
+  //nnow have to find the longest activity and shortest out of the top three
+
+  document.getElementById("longestActivityType").textContent = "run";
+  document.getElementById("shortestActivityType").textContent = "walk";
+
+  /* let RunningWeek = [Sun:0, Mon:0, Tues:0,Wed:0,Thur:0,Fri:0,Sat:0]; */
+  let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let RunningDic = new Map();
+  let BikingDic = new Map();
+  let WalkingDic = new Map();
+  RunningDic.set("Sun", []);
+  RunningDic.set("Mon", []);
+  RunningDic.set("Tue", []);
+  RunningDic.set("Wed", []);
+  RunningDic.set("Thu", []);
+  RunningDic.set("Fri", []);
+  RunningDic.set("Sat", []);
+
+  BikingDic.set("Sun", []);
+  BikingDic.set("Mon", []);
+  BikingDic.set("Tue", []);
+  BikingDic.set("Wed", []);
+  BikingDic.set("Thu", []);
+  BikingDic.set("Fri", []);
+  BikingDic.set("Sat", []);
+
+  WalkingDic.set("Sun", []);
+  WalkingDic.set("Mon", []);
+  WalkingDic.set("Tue", []);
+  WalkingDic.set("Wed", []);
+  WalkingDic.set("Thu", []);
+  WalkingDic.set("Fri", []);
+  WalkingDic.set("Sat", []);
+  for (let i = 0; i < tweet_array.length; i++) {
+    if (tweet_array[i].activityType == "running") {
+      let day = weekDays[tweet_array[i].time.getDay()];
+      let NewArray = RunningDic.get(day); //this is an array
+      NewArray.push(tweet_array[i].distance);
+      RunningDic.set(day, NewArray);
+    } else if (tweet_array[i].activityType == "biking") {
+      let day = weekDays[tweet_array[i].time.getDay()];
+      let NewArray = BikingDic.get(day);
+      NewArray.push(tweet_array[i].distance);
+      BikingDic.set(day, NewArray);
+    } else if (tweet_array[i].activityType == "walking") {
+      let day = weekDays[tweet_array[i].time.getDay()];
+      let NewArray = WalkingDic.get(day);
+      NewArray.push(tweet_array[i].distance);
+      WalkingDic.set(day, NewArray);
+    }
+  }
+
+  document.getElementById("weekdayOrWeekendLonger").textContent = "Sunday";
+
+  let runningData = [];
+  RunningDic.forEach((distance, day) => {
+    runningData.push({ day: day, distance: distance, activity: "running" });
+  });
+
+  let bikingData = [];
+  BikingDic.forEach((distance, day) => {
+    bikingData.push({ day: day, distance: distance, activity: "biking" });
+  });
+
+  let walkingData = [];
+  WalkingDic.forEach((distance, day) => {
+    walkingData.push({ day: day, distance: distance, activity: "walking" });
+  });
+  let combinedActivityData = [];
+  combinedActivityData = combinedActivityData.concat(runningData);
+  combinedActivityData = combinedActivityData.concat(bikingData);
+  combinedActivityData = combinedActivityData.concat(walkingData);
+
+  console.log(combinedActivityData);
+  let expandedActivityData = [];
+
+  combinedActivityData.forEach((entry) => {
+    entry.distance.forEach((distanceValue) => {
+      expandedActivityData.push({
+        day: entry.day,
+        activity: entry.activity,
+        distance: distanceValue,
+      });
+    });
+  });
+  console.log(expandedActivityData);
+  let activity_vis_spec = {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    description:
+      "A graph of the number of Tweets containing each type of activity.",
+    data: {
+      values: activityData,
+    },
+    mark: "bar",
+    encoding: {
+      x: {
+        field: "activity",
+        type: "nominal",
+        axis: { title: "Activity Type" },
+      },
+      y: {
+        field: "tweets",
+        type: "quantitative",
+        axis: { title: "Number of Tweets" },
+      },
+    },
+
+    //TODO: Add mark and encoding
+  };
+  vegaEmbed("#activityVis", activity_vis_spec, { actions: false });
+  let distancePerActivity = {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    description: "A graph of distances for activities by day of the week.",
+    width: 500,
+    height: 400,
+    data: {
+      values: expandedActivityData,
+    },
+    mark: "point",
+    encoding: {
+      x: {
+        field: "day",
+        type: "ordinal",
+        sort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        axis: { title: "Day of the Week" },
+      },
+      y: {
+        field: "distance",
+        type: "quantitative",
+        axis: { title: "Distance" },
+      },
+      color: {
+        field: "activity",
+        type: "nominal",
+        scale: {
+          domain: ["running", "walking", "biking"],
+          range: ["#4287f5", "#f55d42", "#42f57b"],
+        },
+        legend: { title: "Activity Type" },
+      },
+      tooltip: [
+        { field: "day", type: "ordinal" },
+        { field: "distance", type: "quantitative" },
+        { field: "activity", type: "nominal" },
+      ],
+    },
+  };
+  vegaEmbed("#distanceVis", distancePerActivity, { actions: false });
+  //TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
+  //Use those visualizations to answer the questions about which activities tended to be longest and when.
+  let distancePerActivityAggregated = {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    description:
+      "An aggregated graph of average distances for activities by day of the week.",
+    width: 500,
+    height: 400,
+    data: {
+      values: expandedActivityData,
+    },
+    mark: "point",
+    encoding: {
+      x: {
+        field: "day",
+        type: "ordinal",
+        sort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        axis: { title: "Day of the Week" },
+      },
+      y: {
+        field: "distance",
+        type: "quantitative",
+        aggregate: "mean",
+        axis: { title: "Average Distance" },
+      },
+      color: {
+        field: "activity",
+        type: "nominal",
+        scale: {
+          domain: ["running", "walking", "biking"],
+          range: ["#4287f5", "#f55d42", "#42f57b"],
+        },
+        legend: { title: "Activity Type" },
+      },
+      tooltip: [
+        { field: "day", type: "ordinal" },
+        { field: "distance", type: "quantitative", aggregate: "mean" },
+        { field: "activity", type: "nominal" },
+      ],
+    },
+  };
+
+  vegaEmbed("#distanceVisAggregated", distancePerActivityAggregated, {
+    actions: false,
+  });
 }
 
 //Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', function (event) {
-	loadSavedRunkeeperTweets().then(parseTweets);
+document.addEventListener("DOMContentLoaded", function (event) {
+  loadSavedRunkeeperTweets().then(parseTweets);
+
+  distanceVisAggregated.style.display = "none";
+  document
+    .getElementById("aggregate")
+    .addEventListener("click", function (event) {
+      var elem = event.target;
+
+      if (elem.textContent === "Show means") {
+        elem.textContent = "Show all activities";
+        document.getElementById("distanceVis").style.display = "none";
+        document.getElementById("distanceVisAggregated").style.display =
+          "block";
+      } else if (elem.textContent === "Show all activities") {
+        elem.textContent = "Show means";
+        document.getElementById("distanceVis").style.display = "block";
+        document.getElementById("distanceVisAggregated").style.display = "none";
+      }
+    });
 });
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -1,65 +1,128 @@
 class Tweet {
-	private text:string;
-	time:Date;
+  private text: string;
+  time: Date;
 
-	constructor(tweet_text:string, tweet_time:string) {
-        this.text = tweet_text;
-		this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
-	}
+  constructor(tweet_text: string, tweet_time: string) {
+    this.text = tweet_text;
+    this.time = new Date(tweet_time); //, "ddd MMM D HH:mm:ss Z YYYY"
+  }
 
-	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
-    get source():string {
-        //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        if (this.text.includes("completed") || this.text.includes("posted")) {
-            return 'completed_event';
-        } else if (this.text.includes("Achieved") || this.text.includes("Goal")) {
-            return "achievement";
-        } else if (this.text.includes("Watch") || this.text.includes("right now")) {
-            return "live_event";
-        } else {
-            return "miscellaneous";
-        } 
-        
-        
-        
+  //returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
+  get source(): string {
+    //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
+    if (this.text.includes("completed") || this.text.includes("posted")) {
+      return "completed_event";
+    } else if (this.text.includes("Achieved") || this.text.includes("Goal")) {
+      return "achievement";
+    } else if (this.text.includes("Watch") || this.text.includes("right now")) {
+      return "live_event";
+    } else {
+      return "miscellaneous";
     }
+  }
 
-    //returns a boolean, whether the text includes any content written by the person tweeting.
-    get written():boolean {
-        //TODO: identify whether the tweet is written
-        if (this.text.includes("-")) {
-            return true; 
-        } else {
-            return false;
+  //returns a boolean, whether the text includes any content written by the person tweeting.
+  get written(): boolean {
+    //TODO: identify whether the tweet is written
+    if (this.text.includes("-")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get writtenText(): string {
+    if (!this.written) {
+      return "";
+    } else {
+      let UserWrittenPart = this.text.substring(
+        this.text.indexOf("-"),
+        this.text.indexOf("https")
+      );
+      return UserWrittenPart;
+    }
+    //TODO: parse the written text from the tweet
+  }
+
+  get activityType(): string {
+    //have to use space in strings because of conflicting words such as 'ski' would show 'skinned'
+    if (this.source != "completed_event") {
+      return "unknown";
+    }
+    /*  let SentenceWords = this.text.split("");
+    let activityPosition = this.text.search("-") - 1; */
+    if (this.text.includes(" ski ") || this.text.includes(" run ")) {
+      if (this.text.includes(" ski ") && !this.text.includes(" run ")) {
+        return "skiing";
+      } else if (this.text.includes(" run ") && !this.text.includes(" ski ")) {
+        return "running";
+      }
+    } else if (this.text.includes(" bike ") || this.text.includes(" mtn ")) {
+      if (this.text.includes(" bike ") && !this.text.includes(" mtn ")) {
+        return "biking";
+      } else if (this.text.includes(" mtn ")) {
+        return "mountain biking";
+      }
+    } else if (
+      this.text.includes(" walk ") ||
+      this.text.includes(" walking ")
+    ) {
+      return "walking";
+    } else if (this.text.includes(" activity ")) {
+      return "activity";
+    } else if (this.text.includes(" swim ")) {
+      return "swimming";
+    } else if (this.text.includes("workout")) {
+      return "workout";
+    } else if (this.text.includes("yoga")) return "yoga";
+    return "";
+  }
+
+  get distance(): number {
+    if (this.source != "completed_event") {
+      return 0;
+    }
+    if (this.text.includes(" mi ")) {
+      const words = this.text.split(" ");
+      let number = 0;
+      for (const word of words) {
+        const potentialNumber = parseFloat(word);
+        if (!isNaN(potentialNumber)) {
+          number = potentialNumber;
+          break;
         }
-    }
+      }
 
-    get writtenText():string {
-        if(!this.written) {
-            return "";
+      return number;
+    } else if (this.text.includes(" km ")) {
+      const words = this.text.split(" ");
+      let number = 0;
+      for (const word of words) {
+        const potentialNumber = parseFloat(word);
+        if (!isNaN(potentialNumber)) {
+          number = potentialNumber;
+          break;
         }
-        //TODO: parse the written text from the tweet
-        return "";
-    }
-
-    get activityType():string {
-        if (this.source != 'completed_event') {
-            return "unknown";
+      }
+      let toMi = number * 0.621371;
+      return toMi;
+    } else if (this.text.includes("yoga")) {
+      const words = this.text.split(" ");
+      let number = 0;
+      for (const word of words) {
+        const potentialNumber = parseFloat(word);
+        if (!isNaN(potentialNumber)) {
+          number = potentialNumber;
+          break;
         }
-        //TODO: parse the activity type from the text of the tweet
-        return "";
+      }
+      return number;
     }
+    return 0;
+  }
 
-    get distance():number {
-        if(this.source != 'completed_event') {
-            return 0;
-        }
-        //TODO: prase the distance from the text of the tweet
-        return 0;
-    }
-
-    getHTMLTableRow(rowNumber:number):string {
-        //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
-        return "<tr></tr>";
-    }
+  getHTMLTableRow(rowNumber: number): string {
+    //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
+    return "<tr></tr>";
+  }
 }
